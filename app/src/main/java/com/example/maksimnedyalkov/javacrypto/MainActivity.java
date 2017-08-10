48328c6b9428e3905db9e9d60c068f3ce23cace8
Nikolay Georgiev <georgievn@georgievn-m01.vmware.com>
2017-08-10 10:27:22 +0300

    refactored code plus added Monero

--- app/src/main/java/com/example/maksimnedyalkov/javacrypto/MainActivity.java
+++ app/src/main/java/com/example/maksimnedyalkov/javacrypto/MainActivity.java
@@ -36,20 +36,27 @@ private HashMap<String,List<String>> listHash;
         listDataHeader.add("XEM");
         listDataHeader.add("Ethereum");
         listDataHeader.add("Dash");
-        List<String> Bitcoin = new ArrayList<>();
-        Bitcoin.add("Buy / Sell");
+        listDataHeader.add("Monero");
+        List<String> BitcoinList = new ArrayList<>();
+        BitcoinList.add("Buy / Sell");
 
-        List<String> XEM = new ArrayList<>();
-        XEM.add("Buy / Sell");
+        List<String> XEMList = new ArrayList<>();
+        XEMList.add("Buy / Sell");
 
-        List<String> Ethereum = new ArrayList<>();
-        Ethereum.add("Buy / Sell");
+        List<String> EthereumList = new ArrayList<>();
+        EthereumList.add("Buy / Sell");
 
-        List<String> Dash = new ArrayList<>();
-        Dash.add("Buy / Sell");
-listHash.put(listDataHeader.get(0),Bitcoin);
-        listHash.put(listDataHeader.get(1),XEM);
-        listHash.put(listDataHeader.get(2),Ethereum);
-        listHash.put(listDataHeader.get(3),Dash);
+        List<String> DashList = new ArrayList<>();
+        DashList.add("Buy / Sell");
+
+        List<String> MoneroList = new ArrayList<>();
+        MoneroList.add("Buy / Sell");
+
+
+        listHash.put(listDataHeader.get(0),BitcoinList);
+        listHash.put(listDataHeader.get(1),XEMList);
+        listHash.put(listDataHeader.get(2),EthereumList);
+        listHash.put(listDataHeader.get(3),DashList);
+        listHash.put(listDataHeader.get(4),MoneroList);
     }
 }
